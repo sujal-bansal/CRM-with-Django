c55159accaa8223a5de8540d8cd6ca23742264c3
sujal-bansal <bansalsujal2021@gmail.com>
2024-08-22 10:37:53 +0530

    add the rest of the changes

--- leads/urls.py
+++ leads/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import leads_list
+from .views import leads_list, leads_detail
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', leads_list, name='lead-list')
+    path('', leads_list, name='lead-list'),
+    path('<int:pk>/', leads_detail, name='lead-detail'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Lead
 
@@ -8,3 +9,9 @@ def leads_list(request):
     context = {'leads' : leads}
 
     return render(request, "leads/listpage.html", context)
+
+
+def leads_detail(request, pk):
+    return HttpResponse('Here is the detail view')
+
+
